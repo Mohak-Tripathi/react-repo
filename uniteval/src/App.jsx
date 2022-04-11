0eb43535480548d12b912876110417bac6c6ada6
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-04-11 13:02:05 +0530

    Update App.jsx

--- uniteval/src/App.jsx
+++ uniteval/src/App.jsx
@@ -1,39 +1,31 @@
-import "./App.css"
 
-import React, {useState} from  "react";
+import './App.css';
+import React, { useState } from "react";
 function App() {
+  const [inv, setInv] = useState({
+    Score: 76,
+    Wicket: 2,
+    Ball: 50
 
-  const [{ score, setScore}] = useState(76);
-  const [{wicket, setWicket}]= useState(2);
-  const [{ball, setBall}] = useState(50)
+  })
 
-  if(oldScore > 100){
-    return 
+  const handle_cricket = (value, key) => {
+    inv[key] = inv[key] + value;
+    return setInv({
+      Score: inv.Score,
+      Wicket: inv.Wicket,
+      Ball: inv.Ball
+    })
   }
-
-  setScore(oldScore + value)
-
-  const handleWicket= (oldwicket, value) =>{
-    if(score > 100){
-      return;
-    }
-    if(oldwicket >=12){
-      return;
-    }
-
-    setWicket(oldwicket + value);
-  }
-
-
   return (
     <div className="App">
-      <h3 >India:</h3>
-     
+      <h3>India:</h3>
       <div className="banner">
         <div>
           Score:{" "}
           <h1 className="scoreCount">
             {
+              inv.Score
               // show "score" here
             }
           </h1>
@@ -42,6 +34,7 @@ function App() {
           Wicket:{" "}
           <h1 className="wicketCount">
             {
+              inv.Wicket
               // show wicket here
             }
           </h1>
@@ -51,9 +44,8 @@ function App() {
           Over:{" "}
           <h1 className="overCount">
             {
-              // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball
-              // if 1 more ball is thrown then over is now 5.0
-              // you have to write logic to form this string from current ball number.
+              Math.floor(inv.Ball / 6) + "." + inv.Ball % 6
+             
             }
           </h1>
         </div>
@@ -62,24 +54,25 @@ function App() {
       <div className="addScore">
         Add Score
         {/* these buttons should add the respective amount in the score */}
-        <button className="addScore1">Add 1</button>
-        <button className="addScore4">Add 4</button>
-        <button className="addScore6">Add 6</button>
+        <button className="addScore1" onClick={() => handle_cricket(1, "Score")}>Add 1</button>
+        <button className="addScore4" onClick={() => handle_cricket(4, "Score")}>Add 4</button>
+        <button className="addScore6" onClick={() => handle_cricket(6, "Score")}>Add 6</button>
       </div>
 
       <div className="addWicket">
         Add Wicket
         {/* Increase the count of wicket */}
-        <button>Add 1 wicket</button>
+        <button onClick={() => handle_cricket(1, "Wicket")}>Add 1 wicket</button>
       </div>
 
       <div className="addBall">
         Add ball
         {/* Increase the total number of balls thrown here. */}
-        <button>Add 1</button>
+        <button onClick={() => handle_cricket(1, "Ball")}>Add 1</button>
       </div>
 
-      {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+      {/* on" without quotes in h1 tag with class name 'status' */
+      }
     </div>
   );
 }
@@ -88,40 +81,4 @@ export default App;
 
 
 
-
-
-
-
-
-
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-
-// "Hello"
-
-//       {/* <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header> */}
-//     </div>
-//   );
-// }
-
-// export default App;
 
