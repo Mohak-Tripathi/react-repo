fcd327c15d05c7404d239dec6cb82f03612a2583
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-05-01 20:35:49 +0530

    react done
    react done

--- scrimba/scrimba.jsx
+++ scrimba/scrimba.jsx
@@ -283,3 +283,114 @@ export default function Navbar() {
         </nav>
     )
 }
+
+
+
+Class - Prop.quiz
+
+
+
+1. What do props help us accomplish?
+Make a component more reusable.
+
+
+2. How do you pass a prop into a component?
+<MyAwesomeHeader title="???" />
+
+
+3. Can I pass a custom prop (e.g. `blahblahblah={true}`) to a native
+   DOM element? (e.g. <div blahblahblah={true}>) Why or why not?
+No, because the JSX we use to describe native DOM elements will
+be turned into REAL DOM elements by React. And real DOM elements
+only have the properties/attributes specified in the HTML specification.
+(Which doesn't include properties like `blahblahblah`)
+
+
+4. How do I receive props in a component?
+function Navbar(props) {
+    console.log(props.blahblahblah)
+    return (
+        <header>
+            ...
+        </header>
+    )
+}
+
+
+5. What data type is `props` when the component receives it?
+An object!
+
+
+
+
+
+
+
+
+// class- Passing a non-string props
+
+import React from "react"
+import Joke from "./Joke"
+
+/*
+Challenge: Think critically - how would you pass in a prop that wasn't
+a string datatype.
+
+E.g. Say you want each Joke component to receive an "upvotes" and "downvotes"
+prop that is a number, as well as a prop with an array of comments, and a boolean
+of whether the joke is a pun (`isPun`).
+*/
+
+export default function App() {
+    return (
+        <div>
+            <Joke 
+        //string        punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+        //boolean        isPun={true}
+        //number        upvotes={10}
+                downvotes={2}
+        //array        comments={[{author: "", body: "", title: ""}, {...}]}
+            />
+            <Joke 
+                setup="I got my daughter a fridge for her birthday." 
+                punchline="I can't wait to see her face light up when she opens it." 
+                isPun={false}
+            />
+            <Joke 
+                setup="How did the hacker escape the police?" 
+                punchline="He just ransomware!"
+                isPun={true}
+            />
+            <Joke 
+                setup="Why don't pirates travel on mountain roads?" 
+                punchline="Scurvy." 
+                isPun={true}
+            />
+            <Joke 
+                setup="Why do bees stay in the hive in the winter?" 
+                punchline="Swarm." 
+                isPun={true}
+            />
+            <Joke 
+                setup="What's the best thing about Switzerland?" 
+                punchline="I don't know, but the flag is a big plus!" 
+                isPun={false}
+            />
+        </div>
+    )
+}
+
+
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["alice", "bob", "charlie", "danielle"]
+// -->        ["Alice", "Bob", "Charlie", "Danielle"]
+// Your code here
+const capitalized = names.map((name) => {
+    return name[0].toUpperCase() + name.slice(1)
+})
+
+console.log(capitalized)
---
+++ useeffect-restuarant/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { RestaurantDetails } from './Components/RestaurantDetails'
+import { Form } from './Components/Form'
+
+function App() {
+  
+  
+
+  return (
+    <div className="App"> 
+      <Form/>
+    </div>
+  )
+}
+
+export default App
